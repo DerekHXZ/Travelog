c18b5691de71567f37d9c587ede0587c744cd3ba
Derek Xingzhou He <derekhe@gmail.com>
2014-09-13 22:14:17 -0400

    See if it works

--- plaid_client.py
+++ plaid_client.py
@@ -4,18 +4,18 @@ from plaid import Client
 class Plaid:
 
     def __init__(self, clientId, clientSecret):
-         client = Client(client_id=clientId, secret=clientSecret)
+        self.client = Client(client_id=clientId, secret=clientSecret)
 
-    def connect(accountType, username, password, email):
-        connect = client.connect(account_type=accountType, username=username, password=password, email=email)
+    def connect(self, accountType, username, password, email):
+        connect = self.client.connect(account_type=accountType, username=username, password=password, email=email)
         if connect.ok:
             json_response = json.loads(connect.content)
             return json_response['access_token'];
 
-    def answerMFA(accountType, mfaAnswer):
-        step = client.step(account_type=accountType, mfa=mfaAnswer)
+    def answerMFA(self, accountType, mfaAnswer):
+        step = self.client.step(account_type=accountType, mfa=mfaAnswer)
         if step.ok:
             transactions = json.loads(step.content)
 
-        trans = client.transactions
+        trans = self.client.transactions
         print(trans)
